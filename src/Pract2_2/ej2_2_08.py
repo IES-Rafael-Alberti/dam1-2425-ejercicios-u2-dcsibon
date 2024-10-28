7d55660cad428c1aaa24b4269db74e80a85dcf06
dcsibon <dcsibon@gmail.com>
2024-10-28 19:40:39 +0100

    2.08 modificado para que haga más cosas

--- src/Pract2_2/ej2_2_08.py
+++ src/Pract2_2/ej2_2_08.py
@@ -10,23 +10,51 @@ Escribir un programa que pida al usuario un número entero y muestre por pantall
 """
 
 import os
-from ej2_23 import borrarConsola, esPar
+import time
+
+def borrar_consola():
+    if os.name == "posix":
+        os.system ("clear")
+    elif os.name == "ce" or os.name == "nt" or os.name == "dos":
+        os.system ("cls")
 
 
-def comprobarNumero(entrada):
+def es_par(num: int) -> bool:
+    return num % 2 == 0
+
+
+def comprobar_numero(entrada):
     return entrada.replace("-", "").isnumeric()
 
 
-def pedirNumeroEntero(mensaje: str):
+def pedir_numero_entero(mensaje: str) -> int:
     entrada = input(mensaje).replace(" ", "")
     
-    while not comprobarNumero(entrada):
+    while not comprobar_numero(entrada):
         entrada = input("**ERROR**, vuelva a intentarlo\n" + mensaje).replace(" ", "")
     
     return int(entrada)
 
 
-def construirFila(numero, fin, signo):
+def pedir_numero_entero_v2(mensaje: str) -> int:
+    entrada_valida = False
+    while not entrada_valida:
+        entrada = input(mensaje).replace(" ", "")
+        if entrada == "":
+            # Retorno el valor None para marcar que termina el programa...
+            return None
+        else:
+            try:
+                num = int(entrada)
+            except ValueError:
+                print("**ERROR**, vuelva a intentarlo\n")
+            else:
+                entrada_valida = True
+    
+    return num
+
+
+def construir_fila(numero, fin, signo):
     fila = ""
     for i in range(numero, fin, -2):
         fila += str(i * signo) + " "
@@ -34,13 +62,13 @@ def construirFila(numero, fin, signo):
     return fila
 
 
-def construirTriangulo(numero):
+def construir_triangulo(numero):
     if numero > 0:
         signo = 1
     else:
         signo = -1
 
-    if esPar(numero):
+    if es_par(numero):
         inicio = 0
     else:
         inicio = 1
@@ -49,15 +77,31 @@ def construirTriangulo(numero):
 
     triangulo = ""
     for i in range(inicio, numero + 1, 2):
-        triangulo += construirFila(i, inicio - 1, signo) + "\n"
+        triangulo += construir_fila(i, inicio - 1, signo) + "\n"
 
     return triangulo
 
+
 def main():
-    borrarConsola()
-    numero = pedirNumeroEntero("Introduzca un número entero: ")
-    print(construirTriangulo(numero))
+    salir = False    
+
+    while not salir:
+        borrar_consola()
+
+        numero = pedir_numero_entero_v2("Introduzca un número entero: ")
+        
+        if numero is None:
+            salir = True
+        else:
+            print("\n" + construir_triangulo(numero))
+            input("Presione una tecla para continuar...\n")
     
+
+    borrar_consola()
+    print("\n\nBye, bye...\n\n")
+    time.sleep(2)
+    borrar_consola()
+
 
 if __name__ == "__main__":
     main()
