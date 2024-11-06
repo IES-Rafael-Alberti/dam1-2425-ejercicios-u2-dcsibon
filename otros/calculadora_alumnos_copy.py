34e5f7cc4b9388436751f690f2d7dd50f020591c
Diego Cano Sibón <dcsibon@gmail.com>
2024-11-06 01:44:51 +0100

    Update calculadora_alumnos_copy.py

--- otros/calculadora_alumnos_copy.py
+++ otros/calculadora_alumnos_copy.py
@@ -1,14 +1,13 @@
-# ATENCIÓN!!!
-# Importar los paquetes necesarios
-# NO usar print de los errores en nigún sitio, SIEMPRE llamar a la función mostrar_error
-# En comentarios internos tenéis ayuda para guiaros en la resolución de esta práctica.
-# CADA función SOLO puede tener una instrucción return
+# TODO: ATENCIÓN!!!
+# 1. Importar los paquetes necesarios
+# 2. Usar la función mostrar_error para imprimir los errores por consola.
+# 3. Los comentarios TODO os ayudan a resolver cada apartado de esta prueba.
+# 4. CADA función SOLO puede tener una instrucción return.
+# 5. En test_calculadora_alumnos.py crear el test unitario para la función es_resultado_negativo y hacer que todos los test unitarios se cumplan.
+
 
 
 # Mensajes de error predefinidos
-import os
-
-
 MENSAJES_ERROR = (
     "Problemas al intentar limpiar la pantalla {error}",
     "Error al configurar los decimales. Formato: decimales <n>.",
@@ -18,26 +17,37 @@ MENSAJES_ERROR = (
 )
 
 # Operadores soportados por la calculadora
-OPERADORES = "+-x*/:"
+OPERADORES = 
+# TODO: Ayuda: incluye en esta constante los símbolos que reconocerá la aplicación para el cálculo de las operaciones:
+# '+' para la suma.
+# '-' para la resta.
+# 'x' o '*' para la multiplicación.
+# '/' o ':' para la división.
+# '**' o 'exp' para la potencia.
+
 
 
 def limpiar_pantalla():
     """
     Limpia la consola según el sistema operativo.
     """
-    # El desarrollo de esta función está incompleto y con errores...
+    # TODO: El desarrollo de esta función está incompleto y con errores.
     try:
-        os.system('clear' if os.name == 'posix' else 'cls')
+        os.system(clear if os.name = posix else cls)
+        # Otra forma de expresar la misma instrucción sería la siguiente:
+        # if os.name = posix:
+        #     os.system(clear)
+        # else:
+        #     os.system(cls)
     except Exception as e:
-        mostrar_error(0)
+
 
 
 def pausa():
     """
     Pausa la ejecución del programa hasta que se pulse ENTER... "\nPresione ENTER para continuar..."
     """
-    # El desarrollo de esta sin realizar... ver documentación. 
-    input("\nPresione ENTER para continuar...")
+    # TODO: Desarrollar esta función según su documentación. 
 
 
 def mostrar_error(indice_error: int, msj_error = None):
@@ -48,55 +58,38 @@ def mostrar_error(indice_error: int, msj_error = None):
         indice_error (int): Índice del mensaje de error en MENSAJES_ERROR.
         msj_error (str, opcional): Texto adicional para personalizar el mensaje de error.
     """
-    # Completa el código de esta función para que controle específicamente las excepciones IndexError y 
-    # muestre el mensaje: "\n*ERROR* Mensaje de error no definido.\n"
-    # También se pide que se controle cualquier otra excepción que se pueda producir y muestr el mensaje:
-    # "\n*ERROR* Problemas al mostrar error!\n{e}\n"
-    try:
-        if msj_error != None:
-            print(f"\n*ERROR* {MENSAJES_ERROR[indice_error].format(error = msj_error)}\n")
-        else:
-            print(f"\n*ERROR* {MENSAJES_ERROR[indice_error]}\n")
-    except IndexError:
-        print("\n*ERROR* Mensaje de error no definido.\n")
-    except Exception as e:
-        print(f"\n*ERROR* Problemas al mostrar error!\n{e}\n")
+    # TODO: 
+    # 1. Corrige el error que existe a la hora de acceder a los mensajes de error que están en la constante
+    #    MENSAJES_ERROR. A los elementos de una tupla se accede igual que a los caracteres de una 
+    #    cadena de caracteres.
+    # 2. Completa el código de esta función para que controle específicamente las excepciones IndexError y 
+    #    muestre el mensaje: "\n*ERROR* Mensaje de error no definido.\n"
+    # 3. También se pide que se controle cualquier otra excepción que se pueda producir y muestre el mensaje:
+    #    "\n*ERROR* Problemas al mostrar error!\n{e}\n"
+    # 4. En esta función los mensajes de error deben mostrarse con print.
+    if msj_error != None:
+        print(f"\n*ERROR* {MENSAJES_ERROR.format(error = msj_error)}\n")
+    else:
+        print(f"\n*ERROR* {MENSAJES_ERROR}\n")
 
 
-def sumar(num1: float, num2: float) -> float:
-    """
-    Suma dos números y retorna el resultado
-
-    Agrs:
-        num1 (float): primero número a sumar
-        num2 (float): segundo número a sumar
-
-    Returns:
-        (float): resultado de la suma de num1 y num2
-    """
-    # Desarrollo completo, incluida la documentación... recibe 2 números float y retorna la suma de ambos
-    return num1 + num2
+def sumar():
+    # TODO: Realizar el desarrollo completo, incluida la documentación... recibe 2 números float y retorna la suma de ambos.
 
 
-def restar(num1: float, num2: float) -> float:
-    """
-    Resta dos números y retorna el resultado
 
-    Agrs:
-        num1 (float): primero número a sumar
-        num2 (float): segundo número a sumar
-
-    Returns:
-        (float): resultado de la resta de num1 y num2
-    """    
-    # Desarrollo completo, incluida la documentación... recibe 2 números float y retorna la resta de ambos
-    return num1 - num2
+def restar():
+    # TODO: Realizar el desarrollo completo, incluida la documentación... recibe 2 números float y retorna la resta de ambos.
     
 
 
 def es_resultado_negativo(num1: float, num2: float) -> bool:
     """Determina si el resultado de una operación entre num1 y num2 debe ser negativo."""
-    # El desarrollo de esta sin realizar y debe cumplirse la documentación y debe pasar las pruebas unitarias.
+    # TODO: Realizar el desarrollo completo de esta función teniendo en cuenta la documentación y completándola también. 
+    # Debe pasar las pruebas unitarias.
+    # Se trata de una función que os puede venir bien para utilizarla tanto en la función multiplicar, como en dividir.
+    # Ya que va a determinar si la multiplicación o división entre dos números debería ser de signo negativo o no.
+
 
 
 def multiplicar():
@@ -113,9 +106,16 @@ def multiplicar():
     Note:
         Debe redondear los números recibidos a enteros para trabajar.
     """
-    # El desarrollo de esta sin realizar y debe cumplirse la documentación y debe pasar las pruebas unitarias.
-    # OBLIGATORIO usar un bucle for
-
+    # TODO: 
+    # 1. Realizar el desarrollo completo de esta función teniendo en cuenta la documentación. 
+    # 2. Esta función debe pasar las pruebas unitarias.
+    # 3. No podéis usar el operador de multiplicación de Python para realizar el desarrollo de la misma, 
+    #    es decir, que debéis realizar la multiplicación con SUMAS y/o RESTAS...
+    # 4. Aunque se reciben números de tipo float, debéis redondearlos como números enteros para 
+    #    simplificar esta función, es decir, la operación 4.98 * 3.33, deberá convertirse en 5 * 3.
+    # 5. Tened en cuenta que podéis recibir números negativos, es decir, la operación -5 * -5 = 25
+    # 6. OBLIGATORIO usar un bucle for.
+    # 7. Incluir algún comentario para mejorar la claridad y permitir que otros comprendan el propósito y funcionamiento del código.
 
 
 
@@ -136,16 +136,29 @@ def dividir():
     Note:
         Debe redondear los números recibidos a enteros para trabajar.        
     """
-    # El desarrollo de esta sin realizar y debe cumplirse la documentación y debe pasar las pruebas unitarias.
-    
+    # TODO: 
+    # 1. Realizar el desarrollo completo de esta función teniendo en cuenta la documentación. 
+    # 2. Esta función debe pasar las pruebas unitarias.
+    # 3. No podéis usar el operador de división de Python para realizar el desarrollo de la misma, 
+    #    es decir, que debéis realizar la división con SUMAS y/o RESTAS...
+    # 4. Aunque se reciben números de tipo float, debéis redondearlos como números enteros para 
+    #    simplificar esta función, es decir, la operación 4.98 / 3.33, deberá convertirse en 5 / 3.
+    # 5. Tened en cuenta que podéis recibir números negativos, es decir, la operación -5 / -5 = 1
+    # 6. Incluir algún comentario para mejorar la claridad y permitir que otros comprendan el propósito y funcionamiento del código.
 
 
 def potencia():
-    # El desarrollo de esta sin realizar y tampoco la documentación.
-    # PREMISAS a tener en cuenta:
-    # - Cualquier número elevado a 0 da como resultado 1.
-    # - Para simplificar esta práctica vamos a suponer que un número elevado a un 
-    #   exponente negativo siempre dará 0 (aunque en realidad no es así matemáticamente)
+    # TODO: 
+    # 1. Realizar el desarrollo completo de esta función y su documentación. 
+    # 2. Esta función debe pasar las pruebas unitarias.
+    # 3. PREMISAS a tener en cuenta:
+    #    - Cualquier número elevado a 0 da como resultado 1.
+    #    - Para simplificar esta función, vamos a suponer que un número elevado a un 
+    #      exponente negativo siempre dará 0 (aunque en realidad no es así matemáticamente)
+    # 4. Utiliza la función de multiplicar para realizar los cálculos que te 
+    #    harán falta en esta función (RECUERDA que no puedes usar directamente los operadores 
+    #    de Python para la multiplicación y división).
+    # 5. Incluir algún comentario para mejorar la claridad y permitir que otros comprendan el propósito y funcionamiento del código.
 
 
 def pedir_entrada(msj: str) -> str:
@@ -158,21 +171,38 @@ def pedir_entrada(msj: str) -> str:
     Returns:
         str: Entrada del usuario.
     """
-    # El desarrollo de esta función está incompleto... leer documentación
+    # TODO: El desarrollo de esta función está incompleto, debéis terminarla teniendo en cuenta la documentación
     return input(msj)
 
 
 def calcular_operacion(num1: float, num2: float, operador: str) -> float:
-    # Crear la documentación que está incompleta...
-    # El desarrollo de esta función está incompleto... realiza las llamadas adecuadas a las 
-    # funciones ya creadas para realizar los distintos cálculos.
+    """
+    Realiza la operación especificada entre num1 y num2 dependiendo del valor del operador.
+    
+    Args:
+        num1 (float): Primer número.
+        num2 (float): Segundo número.
+        operador (str): Operador de la operación.
+    
+    Returns:
+        float: Resultado de la operación.
+    """        
+    # TODO: El desarrollo de esta función está incompleto... completadla teniendo en cuenta la documentación
+    # y que debe realizar las llamadas adecuadas a las funciones ya creadas para realizar los distintos 
+    # cálculos... sumar, restar, multiplicar, dividir y potencia.
 
     return resultado
 
 
+
 def obtener_operaciones() -> str:
-    """Devuelve una cadena con la lista de operaciones disponibles en la calculadora."""
-    # El desarrollo de esta función está incompleto
+    """
+    Devuelve una cadena con la lista de operaciones disponibles en la calculadora.
+
+    Returns:
+        (str): cadena de caracteres con la información de las operaciones disponibles.
+    """
+    # TODO: El desarrollo de esta función está incompleto... ver documentación para solucionarla correctamente.
     """
     Operaciones disponibles:
       ce => Reiniciar resultado a 0
@@ -189,6 +219,7 @@ def obtener_operaciones() -> str:
     """
 
 
+
 def realizar_calculo():
     """
     Realiza una secuencia de cálculos solicitando números y operadores al usuario.
@@ -201,20 +232,28 @@ def realizar_calculo():
         float: Resultado final del cálculo o None si se cancela.
 
     Note:
-        * El usuario es guiado para introducir números y operadores secuencialmente para realizar operaciones básicas.
-        * El usuario puede utilizar "resultado" en la secuencia de cálculo para reutilizar el resultado almacenado en la calculadora.
-        * El cálculo finaliza al pulsar <ENTER>, volviendo y actualizando el resultado almacenado de la calculadora con el cálculo realizado.
-        * También podemos escribir "cancelar", volviendo sin realizar ningún cambio en el resultado almacenado de la calculadora.    
+        * Dentro de esta función el usuario puede realizar cálculos secuenciales, es decir, 
+          comenzará introduciendo un número, después un operador, y otro número... a partir 
+          de aquí sobre el resultado acumulado introducirá operador y número para seguir 
+          realizando cálculos (ver ejemplos en README.md de la tarea en el repositorio de GitHub).
+        * El usuario es guiado para introducir números y operadores secuencialmente 
+          para realizar operaciones básicas.
+        * El usuario puede utilizar "resultado" en la secuencia de cálculo para reutilizar el 
+          resultado almacenado en la calculadora.
+        * El cálculo finaliza al pulsar <ENTER>, volviendo y actualizando el resultado almacenado 
+          de la calculadora con el cálculo realizado.
+        * También podemos escribir "cancelar", volviendo sin realizar ningún cambio en el 
+          resultado almacenado de la calculadora.    
     """
-    # El desarrollo de esta función está incompleto... ver documentación para solucionarlo.
+    # TODO: El desarrollo de esta función está incompleto... ver documentación para solucionarla correctamente.
 
     operador = None
     resultado = None
-    realizando_calculo = True
+    realizando_calculos = True
 
     print("\n## Ingrese número, operador, 'resultado', 'cancelar' o <ENTER> para finalizar el cálculo ##\n")
 
-    while realizando_calculo:
+    while realizando_calculos:
         entrada = pedir_entrada(f"\t (Cálculo = {resultado if resultado is not None else 0}) >> ")
         
         if entrada == "cancelar":
@@ -224,7 +263,7 @@ def realizar_calculo():
 
 
         elif entrada in OPERADORES:
-
+            operador = 
 
         else:
             if entrada == "resultado":
@@ -272,13 +311,9 @@ def main():
         
         4. La calculadora sigue ejecutándose hasta que el usuario confirma la salida al ingresar una entrada vacía y pulsar <ENTER>.
         
-        5. Finalmente, se limpia la pantalla, el programa se despide y termina.
+        5. Finalmente, se limpia la pantalla, el programa se despide con el mensaje "\n\nBye, bye...\n\n" y termina.
     """
-    # Corrige los errores y haz que el main funcione correctamente...
-
-    #
-    # Agregar una NUEVA FUNCIONALIDAD a la calculadora: el cálculo del exponente
-    #
+    # TODO: Corrige los errores y haz que el main funcione correctamente...
 
     decimales = 2
     resultado = 0.0
@@ -290,28 +325,29 @@ def main():
         pedir_entrada(f"Operación (RES => resultado) >> ")
 
         if entrada == "":
-            desea_salir = pedir_entrada("¿Desea salir de la calculadora? (s/n) ") == "S"
+            pedir_entrada("¿Desea salir de la calculadora? (s/n) ")
 
         elif entrada == "lista":
-            obtener_operaciones()
+            obtener_operaciones
+            pausa
 
         elif entrada == "ce":
             resultado = 0
 
         elif entrada.startswith("decimales"):
-            try:
-                decimales = int(entrada.split()[1])
-                print(f"Decimales configurados a {decimales}.")
-            except
-                mostrar_error
+            decimales = int(entrada.split()[1])
+            print(f"Decimales configurados a {decimales}.")
             
             pausa()                
 
         elif entrada == "calculo":
             realizar_calculo(decimales, resultado)
 
-            pausa()
+            if resultado_ultimo_calculo != None:
+                resultado = resultado_ultimo_calculo
+
+            pausa
 
         else:
             mostrar_error
-            pausa()
+            pausa
